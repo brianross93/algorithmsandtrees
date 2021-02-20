0d0066107fc0bebf1008b3f3673cf16a349fdf19
Brian Ross <brian.ross@students.makeschool.com>
2021-02-20 01:34:49 -0500

    working more on print function

--- HashTable.py
+++ HashTable.py
@@ -7,7 +7,7 @@ class HashTable:
     self.arr = self.create_arr(size)
 
 
-  # 1️⃣ TODO: Complete the create_arr method.
+  # the create_arr method.
 
   # Each element of the hash table (arr) is a linked list.
   # This method creates an array (list) of a given size and populates each of its elements with a LinkedList object.
@@ -22,7 +22,7 @@ class HashTable:
 
 
 
-  # 2️⃣ TODO: Create your own hash function.
+  # Creates my own hash function.
 
   # Hash functions are a function that turns each of these keys into an index value that we can use to decide where in our list each key:value pair should be stored. 
 
@@ -36,7 +36,7 @@ class HashTable:
     return index
 
 
-  # 3️⃣ TODO: Complete the insert method.
+  # the insert method.
 
   # Should insert a key value pair into the hash table, where the key is the word and the value is a counter for the number of times the word appeared. When inserting a new word in the hash table, be sure to check if there is a Node with the same key in the table already.
 
@@ -44,7 +44,7 @@ class HashTable:
     # we're going to find the index for where our key value pair are placed
     key_hash = self.hash_func(key)
 
-
+    
 
     # were gonna check to see if theres something already there
     if self.arr[key_hash] == None:
@@ -52,7 +52,7 @@ class HashTable:
       return key_hash
     
     # if the spot is taken though....
-    else:
+    elif self.arr[key_hash] == key_hash:
       pointer = (key_hash + 1) % self.size
     # that right there is our movement
       while pointer != key_hash:
@@ -71,9 +71,9 @@ class HashTable:
 
 
 
-  # 4️⃣ TODO: Complete the print_key_values method.
+  # the print_key_values method.
 
-  # Traverse through the every Linked List in the table and print the key value pairs.
+  # Traverses through the every Linked List in the table and print the key value pairs.
 
   # For example: 
   # a: 1
@@ -90,6 +90,7 @@ class HashTable:
       return -1
     else:
       for i in self.arr:
+        
         i.print_nodes()
     
 
